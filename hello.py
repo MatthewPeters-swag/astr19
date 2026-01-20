21758941d352d6cd49de567cd359a6bc2b2f8410
Matthew Peters <matthewpeters.us@icloud.com>
2026-01-20 13:25:32 -0800

    Adding code journal 1:1

---
+++ hello.py
@@ -0,0 +1,11 @@
+def Print():
+	print("Matthew Peters")
+	print("he/him")
+	print("My favorite movie is probably Fellowship of the Ring I love the setting and the adventure.")
+	print("My favorite food is probbaly chicken tikka masala, it tastes good.")
+
+def main():
+	Print()
+
+if __name__=="__main__":
+	main()
